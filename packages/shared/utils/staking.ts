6179ec66310b208462b81362124c22049e5e35c3
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-04-29 10:56:36 +0000

    Document configuration - handling edge cases

--- packages/shared/utils/staking.ts
+++ packages/shared/utils/staking.ts
@@ -17,3 +17,4 @@ export const getStakingJettons = (state: StakingState) => {
     .filter((pool) => !!pool.liquid_jetton_master)
     .map((pool) => pool.liquid_jetton_master!);
 };
+Document configuration - handling edge cases
