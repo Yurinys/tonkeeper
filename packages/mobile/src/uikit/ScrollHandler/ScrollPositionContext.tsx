2c1e52765e6155583b0efd51b9a138c785bbf91d
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-09-17 07:08:20 +0000

    Improve auth - fixing a bug

--- packages/mobile/src/uikit/ScrollHandler/ScrollPositionContext.tsx
+++ packages/mobile/src/uikit/ScrollHandler/ScrollPositionContext.tsx
@@ -42,4 +42,4 @@ export const ScrollPositionProvider = memo(({ children }) => {
     </ScrollPositionContext.Provider>
   );
 });
-Improve database - enhancing logsDocument configuration - refactoring codeFix build - improving security
+Improve database - enhancing logsDocument configuration - refactoring codeFix build - improving securityImprove auth - fixing a bug
