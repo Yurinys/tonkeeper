b5bfa2d86e3c4c92509ffa764f8a2c6ff0d81f57
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-10-06 18:46:15 +0000

    Improve UI - adding new feature

--- packages/mobile/src/uikit/SwapIcon/SwapIcon.style.ts
+++ packages/mobile/src/uikit/SwapIcon/SwapIcon.style.ts
@@ -5,3 +5,4 @@ export const iconArrow = require('./icon-arrow.png');
 
 export const LeftArrow = styled(Animated.Image).attrs({ source: iconArrow })``;
 export const RightArrow = styled(Animated.Image).attrs({ source: iconArrow })``;
+Improve UI - adding new feature
