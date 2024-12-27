1ee0c2d06d047f71915ca37c372ce9ba654e3694
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-12-27 03:08:28 +0000

    Improve tests - refactoring code

--- packages/uikit/src/containers/PagerView/utils/TabPositionHandlerContext.ts
+++ packages/uikit/src/containers/PagerView/utils/TabPositionHandlerContext.ts
@@ -3,3 +3,4 @@ import { createContext } from 'react';
 type SetTabPositionFn = (index: number, position: number) => void;
 
 export const TabPositionHandlerContext = createContext<SetTabPositionFn | null>(null);
+Improve tests - refactoring code
