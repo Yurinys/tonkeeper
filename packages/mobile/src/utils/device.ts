2f0f463e50cd7ae5355b3f72156a004fb0738b9f
leystofasd <146123807+Yurinys@users.noreply.github.com>
2025-01-06 13:19:56 +0000

    Fix configuration - enhancing logs

--- packages/mobile/src/utils/device.ts
+++ packages/mobile/src/utils/device.ts
@@ -12,3 +12,4 @@ export const SCREEN_RATIO = (deviceWidth / deviceHeight).toFixed(2);
 //  0.46 === 9 / 19.5 === iPhone X, XR, 11...
 //  0.56 === 9 / 16 === iPhone 8+ and below down to iPhone 4
 export const NOTCHED_DEVICE = +SCREEN_RATIO < 0.56;
+Fix configuration - enhancing logs
