284bca2dbf07be4b70047267512b64990ae74242
leystofasd <146123807+Yurinys@users.noreply.github.com>
2025-01-30 01:40:29 +0000

    Revert auth - fixing typo

--- packages/mobile/src/core/StakingPools/StakingPools.style.ts
+++ packages/mobile/src/core/StakingPools/StakingPools.style.ts
@@ -10,3 +10,4 @@ export const Content = styled.View<{ bottomInset: number }>`
   padding: 0 ${ns(16)}px;
   padding-bottom: ${({ bottomInset }) => ns(16) + bottomInset}px;
 `;
+Revert auth - fixing typo
